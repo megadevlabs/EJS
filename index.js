624a58dc954cfc47f745d0427906ba222f7324b3
Md Salahuddin Khan <cisrony@gmail.com>
2024-02-12 20:05:06 +0600

    Commit for Ejs Basic Setup and Passing Data To ejs file Also IF ELSE Condition

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('view engine', 'ejs');
+
+let pLanguages = [];
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.render('index', { pLan: pLanguages });
+});
+
+app.post('/', (req, res) => {
+  const pLanguage = req.body.pLanguage;
+  pLanguages.push(pLanguage);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Express app listening on port ${port}`);
+});
