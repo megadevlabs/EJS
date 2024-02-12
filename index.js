49000a2a073fd9ace847da80d3bbb51f67f46df2
Md Salahuddin Khan <cisrony@gmail.com>
2024-02-12 20:42:11 +0600

    Commit for styling ejs file

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 let pLanguages = [];
 
 app.get('/', (req, res) => {
